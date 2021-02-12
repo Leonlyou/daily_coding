33e334e360e7c845d6a4d18c5f8f87803659f9dc
刀叨丶Leo <48111458+Leonlyou@users.noreply.github.com>
2021-02-12 23:54:13 +0800

    Update bubblesort.py

--- algorithm/bubblesort.py
+++ algorithm/bubblesort.py
@@ -1,29 +1,20 @@
 # -*- coding:utf-8 -*-
 # author:yueba
-"""
-选择排序实现
-1.min_pos 和 实际最小值下标对换
-2.第i个数和最小数兑换位置
-"""
 import numpy as np
 import time
-
-
-def select_sort(lis: list):
-    for i in range(len(lis)):
-        min_pos = i
-        for j in range(i, len(lis)):
-            if lis[j] < lis[min_pos]:
-                min_pos = j
-        lis[i], lis[min_pos] = lis[min_pos], lis[i]
-    return lis
-
 
 def random_array(i: int):
     array = np.arange(i)
     np.random.shuffle(array)
     return array
 
+
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+    return array
 
 # 装饰器计算功能运行时间
 def cal_time(fn):
@@ -41,7 +32,7 @@ def cal_time(fn):
 @cal_time
 def check(array):
     array_copy = array.copy()
-    array01 = select_sort(array)
+    array01 = bubble_sort(array)
     array02 = sorted(array_copy)
     if len(array01) == len(array02):
         for i in range(len(array01)):
